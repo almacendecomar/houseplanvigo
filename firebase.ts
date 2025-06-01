0805978b98f3d3a2f4fa81907b326cbcf07be2ae
houseplan <sensordeflujo@gmail.com>
2025-06-01 10:46:58 +0200

    Add files via upload

---
+++ firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyATH4q7rV3oKJnDrQR3Eoem7hcsraJGAeA",
+  authDomain: "decomaralmacen-341eb.firebaseapp.com",
+  projectId: "decomaralmacen-341eb",
+  storageBucket: "decomaralmacen-341eb.firebasestorage.app",
+  messagingSenderId: "368724012558",
+  appId: "1:368724012558:web:09868ba2d22676ce62965f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
