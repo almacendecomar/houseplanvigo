c4c3852270240e7f8cc10c5612520b8fdf8687ac
houseplan <sensordeflujo@gmail.com>
2025-06-03 12:12:40 +0200

    Update firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -1,17 +1,13 @@
-// Import the functions you need from the SDKs you need
+// src/firebase.ts
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyATH4q7rV3oKJnDrQR3Eoem7hcsraJGAeA",
-  authDomain: "decomaralmacen-341eb.firebaseapp.com",
-  projectId: "decomaralmacen-341eb",
-  storageBucket: "decomaralmacen-341eb.firebasestorage.app",
-  messagingSenderId: "368724012558",
-  appId: "1:368724012558:web:09868ba2d22676ce62965f"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const firebaseApp = initializeApp(firebaseConfig);
