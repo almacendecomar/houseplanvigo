22d5364c1052974c4f6846d922d121c077d6cc76
houseplan <sensordeflujo@gmail.com>
2025-06-05 11:10:41 +0200

    Update CalendarFromIcs.tsx

--- src/components/booking/CalendarFromIcs.tsx
+++ src/components/booking/CalendarFromIcs.tsx
@@ -1,4 +1,3 @@
-
 // src/components/booking/CalendarFromIcs.tsx
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
@@ -6,7 +5,12 @@ import { useIcalEvents } from '../../hooks/useIcalEvents';
 import '../../styles/fullcalendar.css';
 
 const CalendarFromIcs = () => {
-  const events = useIcalEvents('https://www.airbnb.es/calendar/ical/44397221.ics?s=d674e41bdb25210ae24b17fc2c27fad0');
+  const { events, loading, error } = useIcalEvents(
+    'https://www.airbnb.es/calendar/ical/44397221.ics?s=d674e41bdb25210ae24b17fc2c27fad0'
+  );
+
+  if (loading) return <p className="text-center mt-10">Cargando eventos...</p>;
+  if (error) return <p className="text-center mt-10 text-red-500">Error cargando el calendario.</p>;
 
   return (
     <div className="p-6 max-w-4xl mx-auto bg-white rounded shadow">
